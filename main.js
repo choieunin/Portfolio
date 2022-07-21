110640eb3656972e99cc37ffb14e0e483e53b671
choeeun-in <cei8645@naver.com>
2022-07-21 14:37:47 +0900

    use strict

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const toggleBtn = document.querySelector('.navbar__toogleBtn');
 const menu = document.querySelector('.navbar__menu');
 const icons = document.querySelector('.navbar__icons');
