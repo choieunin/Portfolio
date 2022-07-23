13fc5c9364318657e59c44613714dd0517efc378
choeeun-in <cei8645@naver.com>
2022-07-23 16:50:34 +0900

    스크롤 구현 및 slide_button(진행중)

--- main.js
+++ main.js
@@ -8,3 +8,10 @@ toggleBtn.addEventListener("click", () => {
     menu.classList.toggle('active');
     icons.classList.toggle('active');
 });
+
+$(document).ready(function($) {
+    $(".scroll_move").click(function(event){
+        event.preventDefault();
+        $('html, body').animate({scrollTop:$(this,hash).offset().top}, 500);
+    });
+});
